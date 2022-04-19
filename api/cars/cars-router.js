1f33006a551ba7ce08d0b68afedf651404341034
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-19 15:21:08 -0500

    cars table migration complete

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,14 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+
+router.get('/', async (req, res) => {
+    try {
+        const data =  await Cars.getAll()
+        res.json(data)
+    } catch(err) {
+        console.log(err)
+    }
+
+})
+
+module.exports = router;
